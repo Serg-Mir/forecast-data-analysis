d2da87523cd78f071f6eb790ed10baf9ed6bb503
Sergii Miroshnykov <it.sergm@gmail.com>
2024-09-13 12:13:25 +0200

    Modified default forecast value comment

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     TOMORROW_IO_API_KEY: str
     TOMORROW_IO_API_URL: HttpUrl = "https://api.tomorrow.io/v4/weather/forecast"
-    FORECAST_HOURS: int = 1  # Fetch 24 hours of forecast data
+    FORECAST_HOURS: int = 1  # Fetch 1 hour of forecast data
 
     class Config:
         env_file = ".env"
